bb1401c564a9e6bcb85eee596c32efc286b4a6e1
LamiumAmplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2021-12-13 15:11:42 +0900

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -69,27 +69,27 @@ if [[ $GPU == *NVIDIA* ]]; then
     sudo apt-get -qq -y install cuda >/dev/null
 elif [[ $GPU == *Advanced* ]]; then
     if [[ $OS_VERSION == 20.04 ]] || [[$OS_VERSION == 18.04 ]]; then
-        if [[ $KERNEL_VERSION == 5.4.* ]]; then
+        if [[ $KERNEL_VERSION == 5.4.* ]] || [[ $KERNEL_VERSION == 5.8.* ]];; then
             result=0
-            output=$(dpkg -s | grep -e "rocm-dkms" >/dev/null) || result=$?
+            output=$(dpkg -s | grep -e "amdgpu-dkms" >/dev/null) || result=$?
             if [[ $result == 0 ]]; then
-                sudo apt-get -qq -y remove rocm-opencl rocm-dkms rocm-dev rocm-utils >/dev/null
+                sudo amdgpu-uninstall >/dev/null
             fi
 
+            # Install rocm
             echo "Install rocm."
             sudo apt-get -qq update >/dev/null
-            sudo apt-get -qq -y install libnuma-dev >/dev/null
             (echo 'ADD_EXTRA_GROUPS=1' | sudo tee -a /etc/adduser.conf) >/dev/null
             (echo 'EXTRA_GROUPS=video' | sudo tee -a /etc/adduser.conf) >/dev/null
             (echo 'EXTRA_GROUPS=render' | sudo tee -a /etc/adduser.conf) >/dev/null
-            sudo usermod -aG video $(whoami) >/dev/null
-            sudo usermod -aG render $(whoami) >/dev/null
-            (wget -qO - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -) >/dev/null
-            (echo 'deb [arch=amd64] https://repo.radeon.com/rocm/apt/debian/ ubuntu main' | sudo tee /etc/apt/sources.list.d/rocm.list) >/dev/null
+            sudo usermod -aG video $LOGNAME >/dev/null
+            sudo usermod -aG render $LOGNAME >/dev/null
+            wget -qO amdgpu-install_all.deb https://repo.radeon.com/amdgpu-install/21.40.2/ubuntu/bionic/amdgpu-install_21.40.2.40502-1_all.deb >/dev/null
+            sudo apt-get install ./amdgpu-install_all.deb >/dev/null
             sudo apt-get -qq update >/dev/null
-            sudo apt-get -qq -y install rocm-dkms >/dev/null
+            sudo amdgpu-install --usecase=rocm
         else
-            echo "ROCm is only supported in 5.4 or 5.6-oem."
+            echo "ROCm is only supported in 5.4 or 5.8."
             echo "ROCm installation will be skipped."
         fi
     fi
