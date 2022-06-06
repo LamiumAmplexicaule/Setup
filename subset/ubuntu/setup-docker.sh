fcf7092d213eccababb1c99f89397e4d42d3f514
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2022-06-07 04:18:27 +0900

    Update setup-docker.sh
    Start on boot & install NVIDIA Container Toolkit

--- subset/ubuntu/setup-docker.sh
+++ subset/ubuntu/setup-docker.sh
@@ -23,3 +23,19 @@ fi
 echo "Install docker."
 sudo apt-get -qq update >/dev/null
 sudo apt-get -qq -y install docker-ce docker-ce-cli containerd.io >/dev/null
+
+# Start on boot 
+sudo systemctl enable docker.service >/dev/null
+sudo systemctl enable containerd.service >/dev/null
+
+# NVIDIA Container Toolkit
+GPU=$(lspci | grep VGA | cut -d ":" -f3)
+if [[ $GPU == *NVIDIA* ]]; then
+    distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+          && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+          && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+                sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+                sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    sudo apt-get -qq update >/dev/null
+    sudo apt-get install -y nvidia-docker2 >/dev/null
+fi
