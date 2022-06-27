6da7fc249bba63c676b4faf073a46d36d6a1609e
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2022-06-27 16:00:32 +0900

    Update setup-docker.sh

--- subset/ubuntu/setup-docker.sh
+++ subset/ubuntu/setup-docker.sh
@@ -31,9 +31,9 @@ sudo systemctl enable containerd.service >/dev/null
 # NVIDIA Container Toolkit
 GPU=$(lspci | grep VGA | cut -d ":" -f3)
 if [[ $GPU == *NVIDIA* ]]; then
-    distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+    distribution=$(. /etc/os-release;echo "$ID$VERSION_ID") \
           && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-          && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+          && curl -s -L "https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list" | \
                 sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
                 sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
     sudo apt-get -qq update >/dev/null
