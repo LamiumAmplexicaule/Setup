ad00ab02f9e4bdeac194b36f1f17ea72085fc998
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2022-06-27 16:03:09 +0900

    Update setup-rocm.sh

--- subset/ubuntu/setup-rocm.sh
+++ subset/ubuntu/setup-rocm.sh
@@ -45,8 +45,8 @@ sudo apt-get -qq update >/dev/null
 (echo 'ADD_EXTRA_GROUPS=1' | sudo tee -a /etc/adduser.conf) >/dev/null
 (echo 'EXTRA_GROUPS=video' | sudo tee -a /etc/adduser.conf) >/dev/null
 (echo 'EXTRA_GROUPS=render' | sudo tee -a /etc/adduser.conf) >/dev/null
-sudo usermod -aG video $LOGNAME >/dev/null
-sudo usermod -aG render $LOGNAME >/dev/null
+sudo usermod -aG video "$LOGNAME" >/dev/null
+sudo usermod -aG render "$LOGNAME" >/dev/null
 wget -qO amdgpu-install_all.deb https://repo.radeon.com/amdgpu-install/22.10.1/ubuntu/focal/amdgpu-install_22.10.1.50101-1_all.deb >/dev/null
 sudo apt-get install ./amdgpu-install_all.deb >/dev/null
 sudo apt-get -qq update >/dev/null
