e635a3fcd86568a6984f276aa4685cc2c67cee9a
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2022-08-19 11:14:34 +0900

    Update setup-cuda.sh

--- subset/ubuntu/setup-cuda.sh
+++ subset/ubuntu/setup-cuda.sh
@@ -39,24 +39,16 @@ fi
 # Install cuda
 case $OS_VERSION in 
     22.04)
-        wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin >/dev/null
-        sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600 >/dev/null
-        sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub >/dev/null
-        sudo add-apt-repository -y "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" >/dev/null
+        wget -q -O cuda-keyring_all.deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb >/dev/null
         ;;
     20.04)
-        wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-        sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-        sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub >/dev/null
-        sudo add-apt-repository -y "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /" >/dev/null
+        wget -q -O cuda-keyring_all.deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb >/dev/null
         ;;
     18.04)
-        wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-        sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-        sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub >/dev/null
-        sudo add-apt-repository -y "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /" >/dev/null
+        wget -q -O cuda-keyring_all.deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb >/dev/null
         ;;
 esac
 
+sudo dpkg -i cuda-keyring_all.deb >/dev/null
 sudo apt-get -qq update >/dev/null
 sudo apt-get -qq -y install cuda >/dev/null
