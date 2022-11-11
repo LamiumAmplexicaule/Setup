c2b65d463c693697b51e591b0986cfa5ff515c90
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2022-11-11 10:36:24 +0900

    Update setup-pytorch.sh

--- subset/ubuntu/setup-pytorch.sh
+++ subset/ubuntu/setup-pytorch.sh
@@ -23,19 +23,25 @@ if [[ $GPU == *NVIDIA* ]]; then
     CUDA_10_2=10.2
     CUDA_11_3=11.3
     CUDA_11_6=11.6
+    CUDA_11_7=11.7
     if [[ $(printf "%s\n%s" "$CUDA_10_2" "$CUDA_VERSION" | sort -V | head -n 1) == $CUDA_10_2 ]]; then
         if [[ $(printf "%s\n%s" "$CUDA_11_3" "$CUDA_VERSION" | sort -V | head -n 1) == $CUDA_11_3 ]]; then
             if [[ $(printf "%s\n%s" "$CUDA_11_6" "$CUDA_VERSION" | sort -V | head -n 1) == $CUDA_11_6 ]]; then
-                # 11.6 <= v
+                if [[ $(printf "%s\n%s" "$CUDA_11_7" "$CUDA_VERSION" | sort -V | head -n 1) == $CUDA_11_7 ]]; then
+                    # 11.7 <= v
+                    pip3 install torch torchvision torchaudio
+                    exit;
+                fi
+                # 11.6 <= v < 11.7
                 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
                 exit;
             fi
             # 11.3 <= v < 11.6
-            pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+            pip3 torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
             exit;
         fi
         # 10.2 <= v < 11.3
-        pip3 install torch torchvision torchaudio
+        pip3 install torch==1.12.1+cu102 torchvision==0.13.1+cu102 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu102
     else
         # v < 10.2
         pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
