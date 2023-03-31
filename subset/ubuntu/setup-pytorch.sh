5e84739d546354fb0cb6d70775eec1cb6383c42c
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2023-03-31 21:14:05 +0900

    Update setup-pytorch.sh

--- subset/ubuntu/setup-pytorch.sh
+++ subset/ubuntu/setup-pytorch.sh
@@ -21,9 +21,9 @@ echo "Install PyTorch."
 if [[ $GPU == *NVIDIA* ]]; then
     CUDA_VERSION=$(nvcc --version | grep release | cut -d ' ' -f 5 | tr -d ',')
     case $CUDA_VERSION in
-        10.[012]* ) pip install torch==1.12.1+cu102 torchvision==0.13.1+cu102 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu102 ;;
-        11.[0123]* ) pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113 ;;
-        11.[456]* ) pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116 ;;
+        10.[012] ) pip install torch==1.12.1+cu102 torchvision==0.13.1+cu102 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu102 ;;
+        11.[0123] ) pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113 ;;
+        11.[456] ) pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116 ;;
         11.8 ) pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 ;;
         * ) pip3 install torch torchvision torchaudio ;;
     esac
