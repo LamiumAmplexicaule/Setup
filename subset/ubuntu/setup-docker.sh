60b1143c3f7d83b0a7b99372e5094bd5f266405a
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2023-04-03 15:04:37 +0900

    Update setup-docker.sh

--- subset/ubuntu/setup-docker.sh
+++ subset/ubuntu/setup-docker.sh
@@ -4,7 +4,7 @@ set -eu
 # Install dependencies
 echo "Install dependencies."
 sudo apt-get -qq update >/dev/null
-sudo apt-get -qq -y install apt-transport-https ca-certificates curl gnupg lsb-release >/dev/null
+sudo apt-get -qq -y install ca-certificates curl gnupg lsb-release >/dev/null
 
 # Remove old
 result=0
@@ -14,15 +14,16 @@ if [[ $result == 0 ]]; then
 fi
 
 # Add pgp key
-(curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg) >/dev/null
+sudo mkdir -m 0755 -p /etc/apt/keyrings >/dev/null
+(curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg) >/dev/null
 
 # Add repository
-(echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list) > /dev/null
+(echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list) > /dev/null
 
 # Install docker
 echo "Install docker."
 sudo apt-get -qq update >/dev/null
-sudo apt-get -qq -y install docker-ce docker-ce-cli containerd.io >/dev/null
+sudo apt-get -qq -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null
 
 # Start on boot 
 sudo systemctl enable docker.service >/dev/null
@@ -37,5 +38,7 @@ if [[ $GPU == *NVIDIA* ]]; then
                 sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
                 sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
     sudo apt-get -qq update >/dev/null
-    sudo apt-get install -y nvidia-docker2 >/dev/null
+    sudo apt-get install -y nvidia-container-toolkit >/dev/null
+    sudo nvidia-ctk runtime configure --runtime=docker >/dev/null
+    sudo systemctl restart docker >/dev/null
 fi
