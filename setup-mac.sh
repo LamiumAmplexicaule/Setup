75eb9ee0a2ba51042fa6d8c43c42ea05f99142e0
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2023-05-15 16:50:11 +0900

    Update setup-mac.sh

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-if [[ ! $(which brew) ]]; then
+if [[ ! $(command -v brew) ]]; then
     echo "Homebrew is not installed."
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "Homebrew has been successfully installed."
@@ -20,7 +20,7 @@ echo "Upgrade homebrew."
 brew upgrade >/dev/null
 
 echo "Install fish plugins."
-if [[ $(which fish) ]]; then
+if [[ $(command -v fish) ]]; then
     mkdir -p ~/.config/fish/ >/dev/null
     cp -n config-mac.fish ~/.config/fish/config.fish >/dev/null
     fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher" >/dev/null
@@ -29,20 +29,33 @@ if [[ $(which fish) ]]; then
 fi
 
 echo "Install hyper terminal plugins."
-if [[ $(which hyper) ]]; then
+if [[ $(command -v hyper) ]]; then
     cp -n .hyper.js ~/.hyper.js
-    hyper i hyper-akari >/dev/null
-    hyper i hyper-tab-icons-plus >/dev/null
-    hyper i hyper-statusline >/dev/null
-    hyper i hyperlinks >/dev/null
-    hyper i hypercwd >/dev/null
+    hyper_plugins=(
+        "hyper-akari"
+        "hyper-tab-icons-plus"
+        "hyper-statusline"
+        "hyperlinks"
+        "hypercwd"
+    )
+    for plugin in "${hyper_plugins[@]}" ; do
+        if ! grep -q "$plugin" ~/.hyper.js; then
+            hyper i "$plugin" >/dev/null
+        fi
+    done
 fi
 
 echo "Install rust."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &>/dev/null
+if [[ ! $(command -v rustup) ]]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &>/dev/null
+fi
 
 echo "Finishup."
 brew cleanup >/dev/null
-(echo "$(which fish)" | sudo tee -a /etc/shells) >/dev/null
-echo "Change default shell to $(which fish)"
-chsh -s "$(which fish)"
+if ! grep -q fish /etc/shells; then
+    (echo "$(which fish)" | sudo tee -a /etc/shells) >/dev/null
+fi
+if ! echo $SHELL | grep -q 'fish'; then
+    echo "Change default shell to $(which fish)"
+    chsh -s "$(which fish)"
+fi
