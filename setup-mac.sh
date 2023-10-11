bc3036aa76b7a109924eff6f6e16456577e8d37e
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2023-10-11 15:07:12 +0900

    Update setup-mac.sh

--- setup-mac.sh
+++ setup-mac.sh
@@ -5,9 +5,9 @@ if [[ ! $(command -v brew) ]]; then
     echo "Homebrew is not installed."
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     if [[ $(uname -m) == 'x86_64' ]]; then
-        eval $(/usr/local/bin/brew shellenv)
+        eval "$(/usr/local/bin/brew shellenv)"
     else
-        eval $(/opt/homebrew/bin/brew shellenv)
+        eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
     echo "Homebrew has been successfully installed."
 else
