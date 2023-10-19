79baf7c676cd497124745a49b0bd994a377f96f6
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2023-10-19 09:57:11 +0900

    Update setup-pytorch.sh

--- subset/ubuntu/setup-pytorch.sh
+++ subset/ubuntu/setup-pytorch.sh
@@ -24,11 +24,12 @@ if [[ $GPU == *NVIDIA* ]]; then
         10.[012] ) pip install torch==1.12.1+cu102 torchvision==0.13.1+cu102 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu102 ;;
         11.[0123] ) pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113 ;;
         11.[456] ) pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116 ;;
+        11.7 ) pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu117 ;;
         11.8 ) pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 ;;
         * ) pip3 install torch torchvision torchaudio ;;
     esac
 elif [[ $GPU == *Advanced* ]]; then
-    pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/rocm5.4.2
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6
 else
     pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
 fi
