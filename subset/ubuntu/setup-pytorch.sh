a756c2925507a42df2afe5267f6f9bbf6c21938f
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2024-02-14 00:05:10 +0900

    Update setup-pytorch.sh

--- subset/ubuntu/setup-pytorch.sh
+++ subset/ubuntu/setup-pytorch.sh
@@ -29,7 +29,7 @@ if [[ $GPU == *NVIDIA* ]]; then
         * ) pip3 install torch torchvision torchaudio ;;
     esac
 elif [[ $GPU == *Advanced* ]]; then
-    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.7
 else
     pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
 fi
