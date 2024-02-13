9d7c965f2ddb45a3fb40916d9cfbc4392eb5d7d5
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2024-02-14 00:03:18 +0900

    Update setup-rocm.sh

--- subset/ubuntu/setup-rocm.sh
+++ subset/ubuntu/setup-rocm.sh
@@ -19,7 +19,7 @@ fi
 # Install dependencies
 echo "Install dependencies."
 sudo apt-get -qq update >/dev/null
-sudo apt-get -qq -y install wget gnupg2 gawk curl >/dev/null
+sudo apt-get -qq -y install wget gnupg2 >/dev/null
 
 # Check version
 OS_VERSION=$(lsb_release -rs)
@@ -42,12 +42,13 @@ echo "Install rocm."
 sudo usermod -aG render "$LOGNAME" >/dev/null
 (echo 'EXTRA_GROUPS=video' | sudo tee -a /etc/adduser.conf) >/dev/null
 sudo usermod -aG video "$LOGNAME" >/dev/null
-curl -fsSL https://repo.radeon.com/rocm/rocm.gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/rocm-keyring.gpg
+sudo mkdir --parents --mode=0755 /etc/apt/keyrings
+wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | gpg --dearmor | sudo tee /etc/apt/keyrings/rocm.gpg > /dev/null
 CODE_NAME=$(lsb_release -cs)
-(echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/rocm-keyring.gpg] https://repo.radeon.com/amdgpu/latest/ubuntu $CODE_NAME main" | sudo tee /etc/apt/sources.list.d/amdgpu.list) >/dev/null
+(echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/latest/ubuntu $CODE_NAME main" | sudo tee /etc/apt/sources.list.d/amdgpu.list) >/dev/null
 sudo apt-get -qq update >/dev/null
 sudo apt-get -qq -y install amdgpu-dkms
-(echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/rocm-keyring.gpg] https://repo.radeon.com/rocm/apt/debian/ $CODE_NAME main" | sudo tee /etc/apt/sources.list.d/rocm.list) >/dev/null
+(echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/latest/ $CODE_NAME main" | sudo tee /etc/apt/sources.list.d/rocm.list) >/dev/null
 (echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' | sudo tee /etc/apt/preferences.d/rocm-pin-600) >/dev/null
 sudo apt-get -qq update >/dev/null
 sudo apt-get -qq -y install rocm-dkms >/dev/null
