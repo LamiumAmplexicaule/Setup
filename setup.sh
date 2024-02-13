10e5d74f3b3adcdd45ba328c1b1c071ba8deaeae
Lamium Amplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2024-02-14 00:10:08 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ name=$(uname -s)
 if [[ $name == 'Darwin' ]]; then
     echo 'Setup for macOS.'
     bash ./setup-mac.sh
-elif [[ $(echo "$name" | cut -c 1-5) == 'Linux' ]]; then
+elif [[ $name == 'Linux' ]]; then
     if [[ -f /etc/lsb-release ]]; then 
         echo 'Setup for Ubuntu.'
         bash ./setup-ubuntu.sh
