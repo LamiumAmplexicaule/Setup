5c512f19257b5c2a4e53d4b36d45e44a8ea68fe5
LamiumAmplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2024-05-31 23:57:08 +0900

    Reformat fish config

--- config-linux.fish
+++ config-linux.fish
@@ -1,7 +1,7 @@
 set -xg fish_greeting
 set -xg FZF_DEFAULT_COMMAND "fd --type file --color=always --follow --hidden --exclude .git"
 set -xg FZF_DEFAULT_OPTS "--ansi"
-  
+    
 alias rm='rm -i'
 alias mv='mv -i'
 alias cp='cp -i'
@@ -9,37 +9,37 @@ alias la='ls -a'
 alias update='sudo apt update && sudo apt upgrade'
 
 if type xclip >/dev/null 2>&1
-  alias pbcopy='xclip -selection c'
-  alias pbpaste='xclip -selection c -o'
+    alias pbcopy='xclip -selection c'
+    alias pbpaste='xclip -selection c -o'
 end
 if type wl-copy >/dev/null 2>&1
-  alias pbcopy='wl-copy'
-  alias pbpaste='wl-paste'
+    alias pbcopy='wl-copy'
+    alias pbpaste='wl-paste'
 end
 
 if test -d $HOME/.local/bin
-        fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/.local/bin
 end
 
 if test -d /usr/local/cuda
-        fish_add_path /usr/local/cuda/bin
-        set -xg LD_LIBRARY_PATH /usr/local/cuda/lib64 $LD_LIBRARY_PATH
+    fish_add_path /usr/local/cuda/bin
+    set -xg LD_LIBRARY_PATH /usr/local/cuda/lib64 $LD_LIBRARY_PATH
 end
 
 if test -d /opt/rocm/
-        fish_add_path /opt/rocm/bin /opt/rocm/rocprofiler/bin /opt/rocm/opencl/bin
-        set -xg LD_LIBRARY_PATH /opt/rocm/lib:/opt/rocm/lib64 $LD_LIBRARY_PATH
+    fish_add_path /opt/rocm/bin /opt/rocm/rocprofiler/bin /opt/rocm/opencl/bin
+    set -xg LD_LIBRARY_PATH /opt/rocm/lib:/opt/rocm/lib64 $LD_LIBRARY_PATH
 end
 
 if test -d /usr/local/go
-        fish_add_path /usr/local/go/bin
+    fish_add_path /usr/local/go/bin
 end
 
 if test -d $HOME/go
-        set -xg GOPATH $HOME/go
-        fish_add_path $GOPATH/bin
+    set -xg GOPATH $HOME/go
+    fish_add_path $GOPATH/bin
 end
 
 if test -d $HOME/.cargo
-        fish_add_path $HOME/.cargo/bin
+    fish_add_path $HOME/.cargo/bin
 end
--- config-mac.fish
+++ config-mac.fish
@@ -12,21 +12,21 @@ alias nproc='sysctl -n hw.logicalcpu'
 alias update='brew update; brew upgrade; brew upgrade --cask'
 
 if test -d /usr/local/bin
-        fish_add_path /usr/local/bin
+    fish_add_path /usr/local/bin
 end
 
 if test -d /usr/local/sbin
-        fish_add_path /usr/local/sbin
+    fish_add_path /usr/local/sbin
 end
 
 if test -d /opt/homebrew/bin
-        fish_add_path /opt/homebrew/bin
+    fish_add_path /opt/homebrew/bin
 end
 
 if test -d /opt/homebrew/sbin
-        fish_add_path /opt/homebrew/sbin
+    fish_add_path /opt/homebrew/sbin
 end
 
 if test -d $HOME/.cargo
-        fish_add_path $HOME/.cargo/bin
+    fish_add_path $HOME/.cargo/bin
 end
