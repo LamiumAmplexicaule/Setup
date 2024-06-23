819499302d737f3d506583c7679ce257bbcd8ed0
LamiumAmplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2024-06-23 23:49:51 +0900

    Add setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,31 @@
+Write-Host "Remove unnecessary packages"
+
+$packages = @(
+  "Clipchamp.Clipchamp",
+  "Disney.37853FC22B2CE",
+  "Microsoft.549981C3F5F10",
+  "Microsoft.BingWeather",
+  "Microsoft.GamingApp ",
+  "Microsoft.Getstarted",
+  "Microsoft.MicrosoftOfficeHub",
+  "Microsoft.MicrosoftSolitaireCollection",
+  "Microsoft.MicrosoftStickyNotes",
+  "Microsoft.Todos",
+  "Microsoft.Windows.Photos",
+  "Microsoft.WindowsAlarms",
+  "Microsoft.WindowsCamera",
+  "Microsoft.WindowsFeedbackHub",
+  "Microsoft.WindowsMaps",
+  "Microsoft.WindowsSoundRecorder",
+  "Microsoft.YourPhone",
+  "Microsoft.ZuneMusic",
+  "Microsoft.ZuneVideo",
+  "MicrosoftTeams",
+  "SpotifyAB.SpotifyMusic",
+  "microsoft.windowscommunicationsapps"
+)
+
+foreach ($package in $packages) {
+  Write-Host "Remove: " $package
+  Get-AppxPackage $package | Remove-AppxPackage
+}
