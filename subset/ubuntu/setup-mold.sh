ebf7a1818fc0c7350588c5090929a40ca01983be
LamiumAmplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2024-10-07 00:37:58 +0900

    Update to use the official script for installing dependencies

--- subset/ubuntu/setup-mold.sh
+++ subset/ubuntu/setup-mold.sh
@@ -26,17 +26,6 @@ if ! is_supported_version "$OS_VERSION" "${SUPPORTED_VERSIONS[@]}"; then
     exit 1
 fi
 
-case $OS_VERSION in 
-    20.04)
-        sudo apt-get -qq update >/dev/null
-        sudo apt-get -qq -y install cmake gcc g++ g++-10 >/dev/null
-        ;;
-    *)
-        sudo apt-get -qq update >/dev/null
-        sudo apt-get -qq -y install cmake gcc g++ >/dev/null
-        ;;
-esac
-
 MOLD_VERSION=$(curl -s https://api.github.com/repos/rui314/mold/releases/latest | jq .tag_name | grep -oE "v([0-9])+\.([0-9])+\.([0-9])+")
 echo "Mold version: ${MOLD_VERSION}"
 
@@ -44,9 +33,11 @@ echo "Mold version: ${MOLD_VERSION}"
 echo "Install mold."
 rm -rf mold
 git clone -q https://github.com/rui314/mold.git >/dev/null
-rm -rf mold/build >/dev/null
-mkdir -p mold/build >/dev/null
-cd mold/build
+cd mold
+./install-build-deps.sh >/dev/null
+rm -rf build >/dev/null
+mkdir -p build >/dev/null
+cd build
 git checkout -q -b "$MOLD_VERSION" >/dev/null
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=c++ .. >/dev/null
 cmake --build . -j $(($(nproc) - 1)) >/dev/null
