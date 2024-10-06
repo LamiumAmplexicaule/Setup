955a1f8e3b54ccb2fcc50a003b8ed4eb30ab4c75
LamiumAmplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2024-10-07 01:13:06 +0900

    Add cuda 12 support

--- subset/ubuntu/setup-pytorch.sh
+++ subset/ubuntu/setup-pytorch.sh
@@ -25,7 +25,9 @@ if [[ $GPU == *NVIDIA* ]]; then
         11.[0123] ) pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113 ;;
         11.[456] ) pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116 ;;
         11.7 ) pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu117 ;;
-        11.8 ) pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 ;;
+        11.8|12.0 ) pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 ;;
+        12.[123] ) pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 ;;
+        12.[456] ) pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124 ;;
         * ) pip3 install torch torchvision torchaudio ;;
     esac
 elif [[ $GPU == *Advanced* ]]; then
