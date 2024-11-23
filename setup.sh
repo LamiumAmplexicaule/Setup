f80d2b85590fad73a3327eb92260adcd9da6002f
LamiumAmplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2024-11-23 18:00:08 +0900

    Refactor OS check to get os_id from /etc/os-release

--- setup.sh
+++ setup.sh
@@ -8,7 +8,8 @@ if [[ $name == 'Darwin' ]]; then
     echo 'Setup for macOS.'
     bash "$SCRIPT_DIR/setup-mac.sh"
 elif [[ $name == 'Linux' ]]; then
-    if [[ -f /etc/lsb-release ]]; then 
+    os_id="$(sed -n 's/^ID="\?\([^"]*\)"\?/\1/p' /etc/os-release)"
+    if [[ "$os_id" == ubuntu ]]; then
         echo 'Setup for Ubuntu.'
         bash "$SCRIPT_DIR/setup-ubuntu.sh"
     else
